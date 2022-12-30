d4d60093ba829e48c3f5d27ce8a1be61741dbb85
pedrold10 <pedrogabriel12423@gmail.com>
2022-12-30 10:44:41 -0300

    Adicionando entidades e DAOs

---
+++ Vendas/src/main/java/io/github/pedro/vendas/DAOs/ClienteDAO.java
@@ -0,0 +1,8 @@
+package io.github.pedro.vendas.DAOs;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteDAO {
+
+}
---
+++ Vendas/src/main/java/io/github/pedro/vendas/entity/Produto.java
@@ -0,0 +1,33 @@
+package io.github.pedro.vendas.entity;
+
+import java.math.BigDecimal;
+
+public class Produto {
+    private Integer id;
+    private String description;
+    private BigDecimal precoUnitario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
